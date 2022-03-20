cdf8c320bdda100f48a9aa7276e33c3ead0fb35a
nomadWebDeveloper <kgjsmart1559@gmail.com>
2022-03-20 19:06:27 +0900

    프록시 패턴

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,12 +1,15 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
+import com.company.design.aop.AopBrowser;
 import com.company.design.proxy.Browser;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.Aclazz;
 import com.company.design.singleton.Bclazz;
 import com.company.design.singleton.SocketClient;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
@@ -15,12 +18,27 @@ public class Main {
 //        Browser browser = new Browser("www.naver.com");
 //        browser.show();
 
-        IBrowser browser = new BrowserProxy("www.naver.com");
-        browser.show();
-        browser.show();
-        browser.show();
-        browser.show();
-        browser.show();
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
+
+       IBrowser aopBrowser = new AopBrowser("www.naver.com",
+               ()-> {
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+
+               },
+               ()-> {
+                    long now = System.currentTimeMillis();
+
+                    end.set(now - start.get());
+
+               }
+
+       );
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
+        aopBrowser.show();
+        System.out.println("loading time : " + end.get());
 
     }
 }
